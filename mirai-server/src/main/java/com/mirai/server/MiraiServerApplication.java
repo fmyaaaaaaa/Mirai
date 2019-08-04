c368aa739e8de5c3142c7656b3b53d91f1236da4
fmyaaaaaaa <fmyaaaaaaa@yahoo.co.jp>
2019-08-04 10:44:24 +0900

    #1 first commit of server

---
+++ mirai-server/src/main/java/com/mirai/server/MiraiServerApplication.java
@@ -0,0 +1,13 @@
+package com.mirai.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiraiServerApplication.class, args);
+	}
+
+}
